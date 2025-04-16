6ad53d0e660e62c44e8a09ec10f03fab36135f08
AlexanderRadkevich <alexander1radkevich2git3hud4@gmail.com>
2025-04-16 09:50:42 +0300

    Update hw11.rs

--- src/homework/hw11.rs
+++ src/homework/hw11.rs
@@ -1,80 +1,71 @@
 use rand::Rng;
+
 fn gen_random_vector(n: usize) -> Vec<i32> {
     let mut rng = rand::thread_rng();
-    (0..n)
-        .map(|_| rng.gen_range(10..100))
-        .collect()
+    (0..n).map(|_| rng.gen_range(10..100)).collect()
 }
-fn min_adjacent_sum(data: &[i32]) -> (i32, usize) {
+
+fn min_adjacent_sum(data: &[i32]) -> (i32, (usize, usize)) {
     if data.len() < 2 {
-        return (0, 0);
+        return (0, (0, 0));
     }
-    data.windows(2)
-        .enumerate()
-        .map(|(i, window)| (window[0] + window[1], i))
-        .min_by_key(|&(sum, _)| sum)
-        .unwrap_or((0, 0))
+
+    let mut min_sum = data[0] + data[1];
+    let mut min_indices = (0, 1);
+
+    for i in 0..(data.len() - 1) {
+        let current_sum = data[i] + data[i + 1];
+        if current_sum < min_sum {
+            min_sum = current_sum;
+            min_indices = (i, i + 1);
+        }
+    }
+
+    (min_sum, min_indices)
 }
-fn print_result(data: &[i32], min_sum: i32, min_index: usize) {
-    print!("indexes:");
+
+fn print_vector_with_min(data: &[i32], min_indices: (usize, usize), min_sum: i32) {
+
+    print!("indexes: ");
     for i in 0..data.len() {
-        print!(" {:2}.", i);
+        print!("{:2}.", i);
     }
     println!();
-    print!("data:   [");
-    for (i, &num) in data.iter().enumerate() {
-        if i > 0 {
-            print!(", ");
+
+    print!("data:    [");
+    for i in 0..data.len() {
+        if i == min_indices.0 || i == min_indices.1 {
+            print!("{:2},", data[i]);
+        } else {
+            print!("{:2}, ", data[i]);
         }
-        print!("{:2}", num);
     }
     println!("]");
-    print!("indexes:");
+
+    print!("         ");
     for i in 0..data.len() {
-        if i == min_index {
-            print!(" \\__");
-        } else if i == min_index + 1 {
-            print!(" __/");
+        if i == min_indices.0 || i == min_indices.1 {
+            if i == min_indices.0 {
+                print!("____");
+            } else {
+                print!("__");
+            }
         } else {
             print!("    ");
         }
     }
     println!();
+
     println!(
-        "min adjacent sum={} at indexes:{},{}", 
-        min_sum, 
-        min_index, 
-        min_index + 1
+        "min_adjacent_sum={}+{}={} at indexes:{}.{}",
+        data[min_indices.0], data[min_indices.1], min_sum, min_indices.0, min_indices.1
     );
-    println!();
 }
+
 fn main() {
-    let vec = gen_random_vector(20);
-    let (min_sum, min_index) = min_adjacent_sum(&vec);
-    print_result(&vec, min_sum, min_index);
-}
-#[test]
-fn test_min_adjacent_sum() {
-    let test_cases = [
-        (
-            vec![45, 87, 49, 64, 50, 37, 45, 72, 55, 64, 90, 86, 60, 54, 78, 72, 83, 44, 89, 22],
-            (82, 5)
-        ),
-        (
-            vec![29, 92, 14, 65, 57, 98, 10, 45, 11, 48, 69, 21, 12, 75, 51, 69, 72, 36, 47, 45],
-            (33, 11)
-        ),
-        (
-            vec![19, 86, 66, 95, 40, 24, 90, 74, 98, 37, 26, 44, 76, 86, 48, 63, 11, 38, 29, 40],
-            (49, 16)
-        ),
-        (
-            vec![30, 18, 68, 87, 99, 81, 88, 45, 34, 79, 81, 79, 93, 55, 26, 24, 32, 55, 59, 97],
-            (48, 0)
-        ),
-    ];
-    for (data, expected) in test_cases.iter() {
-        let result = min_adjacent_sum(data);
-        assert_eq!(result, *expected);
-    }
+    let data = gen_random_vector(20);
+
+    let (min_sum, min_indices) = min_adjacent_sum(&data);
+    
+    print_vector_with_min(&data, min_indices, min_sum);
 }
